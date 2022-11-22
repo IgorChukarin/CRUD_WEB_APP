ee332eb10af77a0c3800a5109b92f65f9d1ca562
Igor <Zigor2356@mail.ru>
2022-11-22 22:24:12 +0300

    cleaning the code

---
+++ java8/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+l
+<src/main/java/com/example/clothingShop/config/MvcConfig.java,1\d\1d3318f0ce913f2feef8f715ddb56080b20bc113
+t
+Dsrc/main/java/com/example/clothingShop/config/WebSecurityConfig.java,7\8\785db6f2128ff04df4b5a348614c3e80eedfe679
--- java8/src/main/java/com/example/clothingShop/GreetingController.java
+++ java8/src/main/java/com/example/clothingShop/GreetingController.java
@@ -6,16 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
 public class GreetingController {
-
 
     @Autowired
     private GoodRepo goodRepo;
@@ -71,35 +66,26 @@ public class GreetingController {
         return "main";
     }
 
+
+
+
     @PostMapping("delete")
     public String delete(@RequestParam Integer id, Map<String, Object> model) {
-        String message;
-        if (id == null) {
-            message = "id is not selected";
-        } else {
+        String message = setMessageForDelete(id);
+        if (id != null) {
             goodRepo.deleteById(id);
-            message = "record deleted";
         }
 
         Iterable<Good> goods = goodRepo.findAll();
-
         model.put("message", message);
         model.put("goods", goods);
         return "main";
     }
 
+
     @PostMapping("filter")
     public String filter(@RequestParam Integer from, @RequestParam Integer to, Map<String, Object> model) {
-        String message;
-        if (from == null && to == null) {
-            message = "no filters set";
-        }else if (from != null && to == null) {
-            message = "from " + from;
-        }else if (from == null && to != null) {
-            message = "up to " + to;
-        }else {
-            message = "from " + from + " to " + to;
-        }
+        String message = setMessageForFilter(from, to);
 
         Iterable<Good> goods = goodRepo.findAll();
         List<Good> filteredGoods = filterGoods(from, to, (ArrayList) goods);
@@ -112,17 +98,10 @@ public class GreetingController {
 
     @PostMapping("update")
     public String update(@RequestParam Integer id, @RequestParam String name, Map<String, Object> model) {
+        String message = setMessageForUpdate(id, name);
         Iterable<Good> goods = goodRepo.findAll();
-
-        String message;
-        if (id == null || name == null) {
-            message = "not all fields are filled in";
-        }
-        else {
-            message = "record updated";
-            goodRepo.findById(id).get().setName(name);
-            goodRepo.saveAll(goods);
-        }
+        setNewNameById(id, name);
+        goodRepo.saveAll(goods);
         model.put("message", message);
         model.put("goods", goods);
         return "main";
@@ -130,20 +109,59 @@ public class GreetingController {
 
 
     private ArrayList filterGoods(Integer priceFrom, Integer priceTo, ArrayList<Good> listOfGoods) {
-        if (priceFrom == null && priceTo == null) {
-            return listOfGoods;
+          if (priceFrom != null && priceTo == null) {
+            listOfGoods = (ArrayList) listOfGoods.stream()
+                    .filter(good -> priceFrom <= good.getPrice())
+                    .collect(Collectors.toList());
+        } else if (priceFrom == null) {
+            listOfGoods = (ArrayList) listOfGoods.stream()
+                    .filter(good -> good.getPrice() <= priceTo)
+                    .collect(Collectors.toList());
+        } else {
+            listOfGoods = (ArrayList) listOfGoods.stream()
+                    .filter(good -> priceFrom <= good.getPrice() && good.getPrice() <= priceTo)
+                    .collect(Collectors.toList());
         }
-        else if (priceFrom != null && priceTo == null) {
-            return (ArrayList) listOfGoods.stream().filter(good -> priceFrom <= good.getPrice())
-                                          .collect(Collectors.toList());
+        return listOfGoods;
+    }
+
+
+    private String setMessageForFilter(Integer from, Integer to) {
+        if (from == null && to == null) {
+            return"no filters set";
+        }else if (from != null && to == null) {
+            return "from " + from;
+        }else if (from == null && to != null) {
+            return "up to " + to;
+        }else {
+            return "from " + from + " to " + to;
         }
-        else if (priceFrom == null && priceTo != null) {
-            return (ArrayList) listOfGoods.stream().filter(good -> good.getPrice() <= priceTo)
-                                          .collect(Collectors.toList());
+    }
+
+
+    private String setMessageForUpdate(Integer id, String name) {
+        if (id != null && name != null) {
+            return "record updated";
+        } else {
+            return "not all fields are filled in";
         }
-        else {
-            return (ArrayList) listOfGoods.stream().filter(good -> priceFrom <= good.getPrice() && good.getPrice() <= priceTo)
-                                          .collect(Collectors.toList());
+    }
+
+
+    private String setMessageForDelete(Integer id) {
+        if (id != null) {
+            return "record deleted";
+        } else {
+            return "not all fields are filled in";
+        }
+    }
+
+
+    private void setNewNameById(Integer id, String name) {
+        if (id != null && name != null) {
+            goodRepo.findById(id).get().setName(name);
         }
     }
 }
+
+//TODO: refresh ids
