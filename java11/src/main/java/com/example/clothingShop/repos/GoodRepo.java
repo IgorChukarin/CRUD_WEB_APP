7de374dae01ccd3be41e9e8c415d1d6442a4f643
IgorChukarin <Zigor2356@mail.ru>
2024-05-15 12:14:26 +0300

    cleaned the project

--- java11/src/main/java/com/example/clothingShop/repos/GoodRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.clothingShop.repos;
-
-import com.example.clothingShop.domain.Good;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GoodRepo extends CrudRepository<Good, Long> {
-}
